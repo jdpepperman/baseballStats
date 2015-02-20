60e3bdbeeccb97063d802779651efdb8e1ceb2fa
Joshua Pepperman <joshuapepperman@gmail.com>
2015-02-20 12:12:16 -0600

    Made it so the lists haveto be the same size before the comparison starts

--- compareBattingData.py
+++ compareBattingData.py
@@ -1,8 +1,10 @@
 import sys
 import cPickle as pickle
+from Player import *
 
 if len(sys.argv) != 3:
-	print("Pass the program the two dates to compare in dd/mm/yyy format")
+	print("Pass the program the two dates to compare in dd-mm-yyy format")
+        exit()
 
 firstDate = "battingDataFile_" + str(sys.argv[1])
 secondDate = "battingDataFile_" + str(sys.argv[2])
@@ -14,9 +16,17 @@ except:
     print("There was a problem with the dates you entered. Please try again with different dates.")
     exit()
 
-firstBatterData.sortBy('score')
-secondBatterData.sortBy('score')
 
 secondBatterData.getPlayer("Jose Altuve").ab = 999
 
+if len(firstBatterData) < len(secondBatterData):
+    while len(secondBatterData) != len(firstBatterData):
+        secondBatterData.addBatter(Player("Placeholder", "NAN", 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0))
+elif len(secondBatterData) < len(firstBatterData):
+    while len(firstBatterData) != len(secondBatterData):
+        firstBatterData.addBatter(Player("Placeholder", "NAN", 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0))
 
+#now they should have an equal number of players
+    
+firstBatterData.sortBy('ab')
+secondBatterData.sortBy('ab')
