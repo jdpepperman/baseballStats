47a28a9209c574ec76dacd5586886ea9672490f2
Joshua Pepperman <joshuapepperman@gmail.com>
2015-03-03 18:38:45 -0600

    moved stat dictionairies to the correct files

--- compareBattingData.py
+++ compareBattingData.py
@@ -6,6 +6,46 @@ if len(sys.argv) != 4:
 	print("Pass the program the two dates to compare in yyyy-mm-dd format followed by the stat to compare")
         exit()
 
+battingStats = {
+        'score' : "Score",
+	'ab' : "At Bats",
+	'r' : "Runs",
+	'h' : "Hits",
+	'b2' : "Doubles",
+	'b3' : "Triples",
+	'hr' : "Home Runs",
+	'rbi' : "Runs Batted In",
+	'sb' : "Stolen Bases",
+	'cs' : "Caught Stealing",
+	'bb' : "Walks",
+	'so' : "Strike Outs",
+	'avg' : "Batting Average",
+	'obp' : "On Base Perentage",
+	'slg' : "Slugging Percentage",
+	'ops' : "OBP + SLG",
+	'war' : "Wins Above Replacement",
+	'rc' : "Runs Created",
+	'rc27' : "Runs Created per 27 Outs",
+	'isop' : "Isolated Power",
+	'seca' : "Secondary Average",
+	'gb' : "Ground Balls",
+	'fb' : "Fly Balls",
+	'g2f' : "Ground Balls to Fly Balls",
+	'ab2hr' : "At Bats per Home Run",
+	'bb2pa' : "Walks per Plate Appearance",
+	'bb2k' : "Walk to Strikeout Ratio",
+	'gp' : "Games Played",
+	'tpa' : "Total Plate Appearances",
+	'pit' : "Number of Pitches",
+	'p2pa' : "Pitches per Plate Appearances",
+	'tb' : "Total Bases",
+	'xbh' : "Extra Base Hits",
+	'hbp' : "Hit By Pitch",
+	'gdp' : "Ground into Double Play",
+	'sh' : "Sacrifice Hits",
+        'sf' : "Sacrifice Flies"
+}
+
 firstDate = "battingDataFile_" + str(sys.argv[1])
 secondDate = "battingDataFile_" + str(sys.argv[2])
 
