4fe50249d86549c2814833fa74767669df0aea9d
Joshua Pepperman <joshuapepperman@gmail.com>
2015-03-03 19:24:19 -0600

    removed status printing from runner

--- runner.py
+++ runner.py
@@ -13,14 +13,14 @@ for stat in battingStats:
 	command = "python /home/joshua/programming/baseball/compareBattingData.py " + dayBeforeYesterday.strftime("%Y-%m-%d")  + " " + yesterday.strftime("%Y-%m-%d") + " " + stat
 
         process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
-        output = output + battingStats[stat] + "\n" + process.stdout.read() + "\n"
+        output = output + process.stdout.read() + "\n"
 
 output = output + "\nPITCHING DATA\n\n"
 for stat in pitchingStats:
 	command = "python /home/joshua/programming/baseball/comparePitchingData.py " + dayBeforeYesterday.strftime("%Y-%m-%d")  + " " + yesterday.strftime("%Y-%m-%d") + " " + stat
 
         process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
-        output = output + pitchingStats[stat] + "\n" + process.stdout.read() + "\n"
+        output = output + process.stdout.read() + "\n"
 
 
 os.system("echo \"" + output + "\" | mail -s \"Baseball Statistics Changes From " + dayBeforeYesterday.strftime("%Y-%m-%d") + " to " + yesterday.strftime("%Y-%m-%d") + "\" joshuapepperman@gmail.com")
